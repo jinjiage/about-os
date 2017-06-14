f8b385de383d93d71d2f09563d792baa1e98d576
yejinlei <4010896@qq.com>
2017-06-14 22:00:24 +0800

    有关Qemu,图片遗漏

---
+++ tool/callgraph_qemu.sh
@@ -0,0 +1,1 @@
+sudo callgraph -f main -F "error_report assert strcmp strncmp strlen printf error_printf exit abort qemu_opt_get qemu_opt_set qemu_find_opts qemu_opts_create strerror qemu_opt_get_number error_report_err memset g_free dup strtok g_strdup strstart snprintf sscanf"
